7d3e11775a4367bb1c4cc0f9f5cdbf5180f740f9
sicoding1120 <daffahafizhfirdaus07@gmail.com>
2024-10-15 13:21:26 +0700

    update token 2

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,90 +1,86 @@
-"use client";
+'use client'
 
-import AuthLayout from "@/layouts/auth";
-import FormFieldElement from "@/components/auth/form-fields";
-import { SignInSchema } from "@/lib/schema.zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import axiosInstance from "@/lib/instance.axios";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { useRouter } from "next/navigation";
+import AuthLayout from '@/layouts/auth'
+import FormFieldElement from '@/components/auth/form-fields'
+import { SignInSchema } from '@/lib/schema.zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import axiosInstance from '@/lib/instance.axios'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { useRouter } from 'next/navigation'
 
 const SignIn = () => {
-  const router = useRouter();
+  const router = useRouter()
   const form = useForm<z.infer<typeof SignInSchema>>({
     resolver: zodResolver(SignInSchema),
-    mode: "all",
+    mode: 'all',
     defaultValues: {
-      email: "",
-      password: "",
-    },
-  });
+      email: '',
+      password: ''
+    }
+  })
 
   const handleOnSubmit = async (e: any) => {
-    e.preventDefault();
+    e.preventDefault()
 
     try {
-      toast.info("waiting response....");
-      const response = await axiosInstance.post(
-        "/auth/login",
-        form.getValues()
-      );
-      toast.success("Sign In Success");
+      toast.info('waiting response....')
+      const response = await axiosInstance.post('/auth/login', form.getValues())
+      toast.success('Sign In Success')
 
-      const accessToken = response.data.data.access_token;
-      const targetDomain = `https://footballstorydash.vercel.app/e/${response.data.data.id}`;
+      const accessToken = response.data.data.access_token
+      const targetDomain = `https://footballstorydash.vercel.app/e/${response.data.data.id}`
 
-      window.addEventListener("message", (event) => {
+      window.addEventListener('message', event => {
         if (event.origin === targetDomain) {
-          console.log("Pesan berhasil diproses oleh Frontend 2:", event.data);
+          console.log('Pesan berhasil diproses oleh Frontend 2:', event.data)
         } else {
-          console.warn("Respons dari origin yang tidak sah:", event.origin);
+          console.warn('Respons dari origin yang tidak sah:', event.origin)
         }
-      });
+      })
       setTimeout(() => {
-        window.postMessage(accessToken, targetDomain);
-        console.log("Token telah dikirim:", accessToken);
-        router.push(targetDomain);
-      }, 2000);
+        window.postMessage(accessToken, 'https://footballstorydash.vercel.app')
+        router.push(targetDomain)
+      }, 2000)
     } catch (error: any) {
       if (error.response) {
         // Respons error dari server
-        toast.error("error response data from server");
+        toast.error('error response data from server')
       } else if (error.request) {
         // Request dikirim tetapi tidak ada respons
-        toast.error("No response received from server");
+        toast.error('No response received from server')
       } else {
         // Error lainnya
-        toast.error("Error during registration");
+        toast.error('Error during registration')
       }
     }
-  };
+  }
 
   return (
     <AuthLayout
-      name={"Sign In"}
+      name={'Sign In'}
       form={form}
       onSubmit={handleOnSubmit}
-      link="/auth/sign-up"
+      link='/auth/sign-up'
     >
       <FormFieldElement
-        placeholder="your email"
+        placeholder='your email'
         form={form.control}
-        name={"email"}
-        typeForm={"normal"}
-        typeInput={"email"}
+        name={'email'}
+        typeForm={'normal'}
+        typeInput={'email'}
       />
       <FormFieldElement
-        placeholder="your password"
+        placeholder='your password'
         form={form.control}
-        name={"password"}
-        typeForm={"normal"}
-        typeInput={"password"}
+        name={'password'}
+        typeForm={'normal'}
+        typeInput={'password'}
       />
       <ToastContainer limit={5} />
     </AuthLayout>
-  );
-};
-export default SignIn;
+  )
+}
+export default SignIn
