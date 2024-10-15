e26e0a9a1f3cbcb1caa9cd2f5533aed15c1939b8
sicoding1120 <daffahafizhfirdaus07@gmail.com>
2024-10-15 13:50:01 +0700

    update token 6

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -31,6 +31,7 @@ const SignIn = () => {
       toast.success('Sign In Success')
 
       const accessToken = await response.data.data.access_token
+      console.log(response.data.data);
       window.postMessage(accessToken, 'https://footballstorydash.vercel.app')
       setTimeout(() => {
         router.push(`https://footballstorydash.vercel.app/e/${response.data.data.id}`
