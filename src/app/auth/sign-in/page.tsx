3adc6f06f96fae6096bd394bc87d3ca245a2dc59
sicoding1120 <daffahafizhfirdaus07@gmail.com>
2024-10-15 14:05:28 +0700

    update 9

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -29,13 +29,15 @@ const SignIn = () => {
       toast.info('waiting response....')
       const response = await axiosInstance.post('/auth/login', form.getValues())
       toast.success('Sign In Success')
-      console.log(response.data.data.access_token)
-      // const accessToken = await response.data.data.access_token
-      window.postMessage(response.data.data.access_token, 'https://footballstorydash.vercel.app',[response.data.data.access_token]
-)
-            setTimeout(() => {
-              router.push(`https://footballstorydash.vercel.app/e/${response.data.data.id}`
+      window.postMessage(
+        response.data.data.access_token,
+        'https://footballstorydash.vercel.app',
+        [response.data.data.access_token]
       )
+      setTimeout(() => {
+        router.push(
+          `https://footballstorydash.vercel.app/e/${response.data.data.id}`
+        )
       }, 2000)
     } catch (error: any) {
       if (error.response) {
