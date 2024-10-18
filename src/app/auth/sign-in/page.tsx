28df2d48d5ea0d227fb9d11fc8508009c936eab9
sicoding1120 <daffahafizhfirdaus07@gmail.com>
2024-10-18 09:51:23 +0700

    update link

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -44,7 +44,7 @@ const SignIn = () => {
         router.push(
           `https://footballstorydash.vercel.app/e/${
             response.data.data.id
-          }?status=${encode("success")}&hash=${encodeURIComponent(encCode)}`
+          }?status=${encodeURIComponent('success')}&hash=${encodeURIComponent(encCode)}`
         )
       }, 2000)
     } catch (error: any) {
